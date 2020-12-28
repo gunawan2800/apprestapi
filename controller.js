694c5e6a7c9b7dec399342c40ed622feb699ff4f
gunawan2800 <gunaduadelapan@gmail.com>
2020-12-28 23:55:07 +0800

    menambahkan routes

--- controller.js
+++ controller.js
@@ -3,5 +3,15 @@ var response=require('./res');
 var connection=require('./koneksi');
 
 exports.index=function(req,res){
-    response.ok("Aplikasi rest ku berhasil")
+    response.ok("Aplikasi rest ku berhasil",res)
+};
+//menampilkan semua data mahaisiswa
+exports.tampilSemua=function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(Error,rows,fileds){
+        if(Error){
+            connection.log(Error)
+        }else{
+            response.ok(rows,res)
+        }
+    })
 };
--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 'use strict';
 module.exports=function(app){
     var jsonku=require('./controller');
-    app.route('/').get(jsonku.index);
+
+    app.route('/')
+    .get(jsonku.index);
+
+    app.route('/tampil')
+        .get(jsonku.tampilSemua);
 }
